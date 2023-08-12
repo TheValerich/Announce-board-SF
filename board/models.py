7e4759cccfc2522d0ea87ce92357ffb377a6956d
TheValerich <aleksey_safonov_90@mail.ru>
2023-08-12 12:15:32 +0300

    First commit

---
+++ board/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Author(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
