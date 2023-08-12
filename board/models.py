a23d3907b1e872f718afca8549d615a6a1a64c29
TheValerich <aleksey_safonov_90@mail.ru>
2023-08-12 15:29:58 +0300

    Added Author,Post,Comment,Category

--- board/models.py
+++ board/models.py
@@ -3,12 +3,20 @@ from django.db import models
 
 
 class Author(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
 
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=64, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Текст")
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name="Автор")
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name="Категория")
 
 
 class Comment(models.Model):
-    pass
+    content = models.TextField(verbose_name="Текст")
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True, verbose_name="Объявление")
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64, verbose_name="Категория")
