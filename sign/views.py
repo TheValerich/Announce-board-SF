d679f52de1fa35c2ab18fbcde0e15f218891ef83
TheValerich <aleksey_safonov_90@mail.ru>
2023-08-15 16:35:14 +0300

    added signup views

--- sign/views.py
+++ sign/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from .models import UserRegisterForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+
+class UserLogoutView(LogoutView):
+    next_page = 'login_url'
+
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = '/posts/'
