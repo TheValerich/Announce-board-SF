8a95421e62a7f37b360d41de0917bd1bcb600016
TheValerich <aleksey_safonov_90@mail.ru>
2023-08-15 16:30:09 +0300

    method __str__, get_url

--- board/models.py
+++ board/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Author(models.Model):
@@ -12,11 +13,21 @@ class Post(models.Model):
     author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name="Автор")
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name="Категория")
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     content = models.TextField(verbose_name="Текст")
     post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True, verbose_name="Объявление")
 
+    def get_absolute_url(self):
+        return reverse('posts_url')
+
 
 class Category(models.Model):
     category = models.CharField(max_length=64, verbose_name="Категория")
+
+    def __str__(self):
+        return self.category
+
