8026eea878355561c8811f44d172505177adcdc5
TheValerich <aleksey_safonov_90@mail.ru>
2023-08-15 16:34:53 +0300

    added CommentCreateView

--- board/views.py
+++ board/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import CreateView, ListView, UpdateView
-from .models import Post
+from .models import Post, Comment
 
 
 class PostsListView(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = 'comment.html'
+    context_object_name = 'comment'
+    fields = [
+        'content',
+    ]
